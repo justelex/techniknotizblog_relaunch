38f052d185aa47706960df8152670eed2e644fab
justelex <maik.nauheim@googlemail.com>
2021-05-24 10:20:22 +0200

    added default nix stup

---
+++ default.nix
@@ -0,0 +1,28 @@
+# This imports the nix package collection,
+# so we can access the `pkgs` and `stdenv` variables
+with import <nixpkgs> {};
+
+# Make a new "derivation" that represents our shell
+stdenv.mkDerivation {
+  name = "FFWW_WEB_BULDER";
+
+  # The packages in the `buildInputs` list will be added to the PATH in our shell
+  buildInputs = [
+    # cowsay is an arbitary package
+    # see https://nixos.org/nixos/packages.html to search for more
+    pkgs.cowsay
+    pkgs.figlet
+    pkgs.lolcat
+	pkgs.ruby
+	pkgs.bundler
+	pkgs.jekyll
+	pkgs.nodejs
+	rubyPackages_2_5.json
+	pkgs.zlib
+	#pkgs.python38Packages.setuptools
+ ];
+	  shellHook = ''
+    figlet "Autoanalyze  DEV ENV" | lolcat --freq 0.5
+  '';
+}
+
